13ab42be7236aeb0ac7ec4f72652b9c78d81b74e
Noman ali <92971191+NomanAli12@users.noreply.github.com>
2021-12-10 17:34:30 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import users from './components/Users';
+import UserFinder from "./components/UserFinder";
+
+function App() {
+  return (
+    <div>
+      <UserFinder />
+    </div>
+  );
+}
+
+export default App;
